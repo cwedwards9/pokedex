f6c1f32e343ee5acc3a4eefb043b459c7d880328
Chase Edwards <chase.w.edwards9@gmail.com>
2021-07-11 12:14:04 -0400

    Add navbar and moves list section.

--- src/pages/PokemonDetail.js
+++ src/pages/PokemonDetail.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, NavLink } from "react-router-dom";
 import axios from "axios";
+import getTypeBgColor from "../utils/getTypeBgColor";
 import styles from "./PokemonDetail.module.scss";
 
-const POKE_API = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/";
+const POKE_API = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/";
 
 export default function PokemonDetail() {
     const { id } = useParams();
@@ -30,15 +31,47 @@ export default function PokemonDetail() {
 
     return(
         <section className={styles.pokemonDetails}>
-            <h1>{pokemon.name && capitalizeString(pokemon.name)} 
+            <nav className={styles.navbar}>
+                <ul>
+                    <li><NavLink to={`/pokemon/${parseInt(id) - 1}`}>#{pokemon.id && addZero(parseInt(pokemon.id - 1))}</NavLink></li>
+                    <li><NavLink to={`/pokemon/${parseInt(id) + 1}`}>#{pokemon.id && addZero(parseInt(pokemon.id + 1))}</NavLink></li>
+                </ul>
+            </nav>
+            <h1 className={styles.pokemonName}>{pokemon.name && capitalizeString(pokemon.name)} 
                 <span> #{pokemon.id && addZero(pokemon.id)}</span>
             </h1>
             <div className={styles.pokemonGrid}>
                 <img className={styles.pokeImage} src={imgSrc} alt={pokemon.name} />
-                <div className={styles.pokeAbilities}>
-                    <h3>Abilities</h3>
-                    <p>{pokemon.abilities && pokemon.abilities[0].ability.name}</p>
+                <div className={styles.pokeInfo}>
+                    <div className={styles.pokeInfoCard}>
+                        <div className={styles.pokeMoves}>
+                            <h3>Moves</h3>
+                            <div className={styles.movesList}>
+                                {pokemon.moves && pokemon.moves.slice(0, 5).map(move => (
+                                    <p key={move.move.name} className={styles.move}>{move.move.name}</p>
+                                ))}
+                            </div>
+                        </div>
+                        <div className={styles.pokeAbilities}>
+                            <h3>Abilities</h3>
+                            <div>
+                                {pokemon.abilities && pokemon.abilities.map(ab => (
+                                    <p key={ab.ability.name}>{ab.ability.name}</p>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={styles.pokeTypes}>
+                        <h3>Type</h3>
+                        <div className={styles.typesList}>
+                            {pokemon.stats && pokemon.types.map(type => (
+                                <p key={type.slot} className={styles.type} style={{backgroundColor: getTypeBgColor(type.type.name)}}>{type.type.name}</p>
+                            ))}
+                        </div>
+                    </div>
                 </div>
+                
                 <div className={styles.pokeStats}>
                     <h3>Stats</h3>
                     <div className={styles.statsList}>
@@ -47,14 +80,6 @@ export default function PokemonDetail() {
                                 <p>{stat.stat.name}</p>
                                 <p>{stat.base_stat}</p>
                             </div>
-                        ))}
-                    </div>
-                </div>
-                <div className={styles.pokeTypes}>
-                    <h3>Types</h3>
-                    <div className={styles.typesList}>
-                        {pokemon.stats && pokemon.types.map(type => (
-                            <p key={type.slot}>{type.type.name}</p>
                         ))}
                     </div>
                 </div>
