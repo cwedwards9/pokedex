a646a358fe2e7469cc7699a61abaf3f93aa9c77b
Chase Edwards <chase.w.edwards9@gmail.com>
2021-08-01 13:52:15 -0400

    Add logic for handling previous/next pokemon links for the first listed pokemon and the last pokemon.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,10 +9,16 @@ export default function Navbar({id}) {
 
     return (
         <nav className={styles.navbar}>
-                <ul>
-                    <li><NavLink to={`/pokemon/${parseInt(id) - 1}`}>#{id && addZero(parseInt(id - 1))}</NavLink></li>
-                    <li><NavLink to={`/pokemon/${parseInt(id) + 1}`}>#{id && addZero(parseInt(id + 1))}</NavLink></li>
-                </ul>
+            <ul>
+                { addZero(parseInt(id - 1)) <= 0 
+                    ? <li><NavLink className={styles.link} to={`/pokemon/898`}>#898</NavLink></li>
+                    : <li><NavLink className={styles.link} to={`/pokemon/${parseInt(id) - 1}`}>#{id && addZero(parseInt(id - 1))}</NavLink></li> 
+                }
+                { addZero(parseInt(id + 1)) >= 899
+                    ?  <li><NavLink className={styles.link} to={`/pokemon/1`}>#001</NavLink></li>
+                    : <li><NavLink className={styles.link} to={`/pokemon/${parseInt(id) + 1}`}>#{id && addZero(parseInt(id + 1))}</NavLink></li> 
+                }
+            </ul>
         </nav>
     );
 }
